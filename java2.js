4cc2e7ecac234a359bf9b4ef42076fc1e358cf8f
seren91 <alexxisra.com>
2023-11-25 23:26:05 +0200

    new3

---
+++ java2.js
@@ -0,0 +1,26 @@
+
+for( let j = 1; j <= 101; j++ )
+{   
+    str = '';       
+    
+    if( j%3 == 0 )
+    {
+        str += 'Fizz';
+    } 
+
+    if( j%5 == 0 )
+    {
+        str += 'Buzz';
+    }   
+
+    
+    console.log( str ? str : j );
+}
+
+
+
+for( let j = 1; j <= 20; j++ )
+{   
+    str = ''; 
+    console.log( str ? str : j );
+}
---
+++ numbers.js
@@ -0,0 +1,28 @@
+
+let num1=240,num2=12,num3=32,num4=45.
+if (num1>num2 && num1>num3 && num1>num4 ) x=num1 ;
+          else if (num2>num3 && num2>num4) x= num2;
+          else if (num3>num1&& num3>num2 && num3>num4) x= num3;
+          else if (num4>num1 && num4>num2 && num4>num3) x= num4;
+     console.log (x)
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
